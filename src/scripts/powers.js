1c27a686da1f7b946d724416a442772385623371
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-16 15:59:09 -0500

    Individual power buttons work, stuck on activate all

--- src/scripts/powers.js
+++ src/scripts/powers.js
@@ -1,22 +1,35 @@
 // Practice: Wonder Powers Activate!
 // The learning objective of this exercise is writing event listeners to handle button clicks, and modifying the classList of DOM elements when those events are broadcast by the browser.
+const flight = document.querySelector("#flight")
+const mindreading = document.querySelector("#mindreading")
+const xray = document.querySelector("#xray")
 
+const powerButtons = [flight, mindreading, xray]
 
-// Add the correct string as the first argument to addEventListener()
-document.querySelector("#activate-flight").addEventListener()
+const togglePowers = (power) => {
+    power.classList.toggle("enabled")
+}
 
-// Write a function named flightHandlerFunction that will remove the disabled class on the corresponding <section id="flight"> (section, not button) and replace it with a class of enabled.
-// Have your developer tools open. When you click the button, the following element...
-// <section id="flight" class="power disabled">
-// Should then look like this.
+document.querySelector("#activate-flight").addEventListener("click", function (){
+    togglePowers(flight)
 
-// <section id="flight" class="power enabled">
-// Once you have that working, write two more JavaScript functions, and two more querySelector().addEventListener() statements for handling the click event on the other two buttons. Those event handlers should perform the same step of removing disabled, and adding enabled on the corresponding <section> elements
+})
 
-// document.querySelector("#activate-mindreading").addEventListener()
-// document.querySelector("#activate-xray").addEventListener()
-// Once that is complete, add two more buttons
+document.querySelector("#activate-mindreading").addEventListener("click", function () {
+    togglePowers(mindreading)
+})
+
+document.querySelector("#activate-xray").addEventListener("click", function () {
+    togglePowers(xray)
+})
+
 
 // Enable All Powers
+/// THIS IS WHERE I STOPPED WORKING ON THIS SO FAR
+document.querySelector("#activate-all").addEventListener("click", function (){
+    powerButtons.forEach(element => {
+        element.classList.add("enabled") // is working as toggle currently...
+    })
+})
 // Disable All Powers
 // Write two more event handlers for activating and deactivating all powers when the corresponding buttons are clicked. You will need to use the document.querySelectorAll() method for these.
