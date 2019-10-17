7ddaf7b44338c7180e5ac23c2dd36bd496ea3f64
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-17 10:13:18 -0500

    re-factored code so that one function adds event listeners to the individual power buttons

--- src/scripts/powers.js
+++ src/scripts/powers.js
@@ -1,40 +1,43 @@
 // Practice: Wonder Powers Activate!
 // The learning objective of this exercise is writing event listeners to handle button clicks, and modifying the classList of DOM elements when those events are broadcast by the browser.
-const flight = document.querySelector("#flight")
-const mindreading = document.querySelector("#mindreading")
-const xray = document.querySelector("#xray")
 
-// all them buttons
-const powerButtons = document.querySelectorAll(".power")
+// creating references to the individual power buttons
+const flightButton = document.getElementById("activate-flight")
+const mindreadingButton = document.getElementById("activate-mindreading")
+const xrayButton = document.getElementById("activate-xray")
 
-const togglePowers = (power) => {
-    power.classList.toggle("enabled")
+// all them powers and button
+const powers = document.querySelectorAll(".power")
+const individualButtons = [flightButton, mindreadingButton, xrayButton]
+
+
+// trying out .split
+// targetPower takes a string and targets the element that has an id of that string
+const targetPower = (powerName) => {
+    return document.getElementById(`${powerName}`)
 }
 
-document.querySelector("#activate-flight").addEventListener("click", function (){
-    togglePowers(flight)
-
+// adding event listeners to all individual power button
+individualButtons.forEach(button => {
+    button.addEventListener("click", function (){
+        // getting a string with the name of the power. For example, "#activate-flight" button will have the powerName of "flight"
+        powerName = event.target.id.split("-")[1]
+        // targeting the section with an id that equals the string in powerName
+        currentPower = targetPower(powerName)
+        // toggling the power
+        currentPower.classList.toggle("enabled")
+    })
 })
-
-document.querySelector("#activate-mindreading").addEventListener("click", function () {
-    togglePowers(mindreading)
-})
-
-document.querySelector("#activate-xray").addEventListener("click", function () {
-    togglePowers(xray)
-})
-
 
 // Enable All Powers
 document.querySelector("#activate-all").addEventListener("click", function (){
-    powerButtons.forEach(button => {
-        button.classList.add("enabled") 
+    powers.forEach(power => {
+        power.classList.add("enabled") 
     })
 })
 // Disable All Powers
 document.querySelector("#deactivate-all").addEventListener("click", function (){
-    powerButtons.forEach(button => {
-        button.classList.remove("enabled")
+    powers.forEach(power => {
+        power.classList.remove("enabled")
     })
 })
-// Write two more event handlers for activating and deactivating all powers when the corresponding buttons are clicked. You will need to use the document.querySelectorAll() method for these.
